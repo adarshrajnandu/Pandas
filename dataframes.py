7b1f05ccb205dfbbef3eb7f3764ce715814e7acf
Adarsh R <adarshraj1003@gmail.com>
2020-06-20 11:58:26 +0530

    Data frame indexing and using cross section for multi level index DF

--- dataframes.py
+++ dataframes.py
@@ -50,3 +50,26 @@ df.set_index('States')
 df
 df.set_index('States',inplace = True)
 df
+
+
+#Index levels
+
+outside = 'G1 G1 G1 G2 G2 G2'.split()
+inside = [1,2,3,1,2,3]
+hier_index =  list(zip(outside,inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+
+outside
+inside
+hier_index
+ df = pd.DataFrame(randn(6,2),hier_index,['A','B'])
+df
+df.loc['G1']
+df.loc['G1'].loc[1]
+df.index.names = ['Groups','Nums']
+df
+df.loc['G2'].loc[2]['B']
+
+#cross selection indexing
+
+df.xs(1,level = 'Nums')
