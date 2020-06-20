0c1dc9c2be7795bde2c552bd1c6c1743fa42cdf1
Adarsh R <adarshraj1003@gmail.com>
2020-06-20 07:20:03 +0530

    Added how to select rows and subset dataframes also performed the drop fucnction

--- dataframes.py
+++ dataframes.py
@@ -5,12 +5,25 @@ from numpy.random import randn
 np.random.seed(101)
 
 df = pd.DataFrame(randn(5,4),['A','B','C','D','E'],['W','X','Y','Z'])
-print(df['W'])
+df['W']
 type(df['W'])
 
 df['new'] = df['W'] + df['Y']
-print(df)
+df
 
 df.drop('new',axis = 1)
 df
-df.drop('new',axis = 1,inplace = True
+
+# inplace is used to permanently remove the changes to the DataFrame
+df.drop('new',axis = 1,inplace = True)
+df
+
+df.drop('E')
+df
+
+
+# selecting rows
+df.loc['A']
+df.iloc[0]
+df.loc['B','Y']
+df.loc[['A','B'],['W','Y']]
