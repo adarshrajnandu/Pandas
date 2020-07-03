70b050240fee9311c9f9ee567caa38b954d22823
Adarsh R <adarshraj1003@gmail.com>
2020-07-03 15:45:21 +0530

    Added more operations for manipulating datasets

--- Pandas_Full.py
+++ Pandas_Full.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 df = pd.read_csv('pokemon_data.csv')
 df.head()
@@ -41,14 +42,56 @@ df.sort_values(['Name','Attack','Defense'],ascending=[1,0,0]) #1 = True and 0 = 
 
 #Operation on dataset
 
-df['Special Power'] = df['Sp. Atk'] + df['Sp. Def']
+df['Special Power'] = df['Sp. Atk'] + df['Sp. Def'] # adding a new column
 df.head()
 df.drop(columns=['Sp. Atk', 'Sp. Def'])
 df.head()
-df.drop(columns='Special Attack',inplace=True) # add inplace=True to make the change permanent
+df.drop(columns='Special Power',inplace=True) # add inplace=True to make the change permanent
 df.head()
-df['Total Power'] = df.iloc[:,4:10].sum(axis = 1)
+df['Total Power'] = df.iloc[:,4:10].sum(axis = 1) # adding a new column using iloc
 df.head()
+
+df.insert(9,'Special Power', (df['Sp. Atk'] + df['Sp. Def'])) #add column at any position
+# df.drop(columns='Special Power',inplace=True) # add inplace=True to make the change permanent
+# df.head()
+df.insert(9,'Special Power',df.iloc[:,7:9].sum(axis = 1))
+# df.drop(columns='Special Power',inplace=True) # add inplace=True to make the change permanent
+# df.head()
+df.set_index('Name',inplace=True)
+df.drop(['Bulbasaur','Ivysaur','Venusaur'])
+
+df_new = pd.DataFrame({'Name':['Raj','Mia','Sree'],'Age': [20,22,24],'Place': ['KL','MH','UP']})
+df_new
+index_ = pd.date_range(start='2020-01-01',periods=3,freq='M')
+df_new.index = index_
+df_new
+df_trunc = df_new.truncate(after='20200229') #Truncate DataFrame
+df_trunc
+df_trunc = df_new.truncate(before ='20200229')
+df_trunc
+
+df_iter = pd.DataFrame( {'name':["aparna", "pankaj", "sudhir", "geeku"],
+                         'degree': ["MBA", "BCA", "M.Tech", "MBA"],
+                         'score':[90, 40, 80, 98] } )
+df_iter
+for i, j in df_iter.iterrows(): #iterating over rows
+    print(j,'\n')
+
+#Handling missing data with pandas
+df_null = pd.DataFrame( {'First':[100, 90, np.nan, 95],
+        'Second': [30, 45, 56, np.nan],
+        'Third':[np.nan, 40, 80, np.nan]} )
+df_null
+df_null.isnull() #checking for missing valiues using isnull()
+bool_Series = pd.isnull(df_null['Third'])
+bool_Series
+df_null[bool_Series] #use ~ for not null values
+
+df_null.notnull() #check using notnull
+# In order to fill null values in a datasets, we use fillna() function
+df_null.fillna(method='ffill')
+df_null.fillna(method='bfill')
+
 
 #Filtering data
 df.head()
